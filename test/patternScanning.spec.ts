1d896822fde66837fef196d5ea7bac1ffc746b16
alfman99 <43989698+alfman99@users.noreply.github.com>
2023-08-30 20:36:29 +0200

    first version test pattern scanning

--- test/patternScanning.spec.ts
+++ test/patternScanning.spec.ts
@@ -1,4 +1,5 @@
 import { PatternScanning } from '../src/patternScanning';
+import { OpenProcess } from '../src/process';
 
 // modify timeout to 10 minutes
 jest.setTimeout(600000);
@@ -6,6 +7,10 @@ const PROCESS_NAME = 'notepad.exe';
 
 describe('patternscanning module', () => {
   test('Get module handle', () => {
-    PatternScanning();
+    const handle = OpenProcess(PROCESS_NAME);
+
+    const addr = PatternScanning(handle, '69 69 69', 0x0, 0x9999);
+
+    console.log(addr);
   });
 });
