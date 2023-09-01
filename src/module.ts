edd9d4ec589b5f699259ebc3b36d37ca5ef58957
alfman99 <amanresamntz@gmail.com>
2023-09-01 16:19:30 +0200

    module functions accept pid, name & handle

--- src/module.ts
+++ src/module.ts
@@ -1,34 +1,67 @@
 import {
-  JSLPMODULEINFO,
   JSMODULEENTRY32,
+  Jslpmoduleinfo,
   getModuleHandle,
   getModuleInformation,
   getProcessModuleEntry32,
   listProcessModules,
+  processHandleToName,
+  processHandleToPid,
+  processNameToPid,
+  processPidToName,
 } from '../../base-memoryts';
 
 export function FindModule(
-  process_name: string,
+  process: string | number | ProcessHandle,
   module_name: string
 ): JSMODULEENTRY32 {
-  return getProcessModuleEntry32(process_name, module_name);
+  if (typeof process === 'object') {
+    process = processHandleToName(process);
+  } else if (typeof process === 'number') {
+    process = processPidToName(process);
+  }
+
+  return getProcessModuleEntry32(process, module_name);
 }
 
-export function ListModules(process_pid: number): Array<JSMODULEENTRY32> {
-  return listProcessModules(process_pid);
+export function ListModules(
+  process: number | string | ProcessHandle
+): Array<JSMODULEENTRY32> {
+  // If process is a ProcessHandle, convert it to a pid
+  if (typeof process === 'object') {
+    process = processHandleToPid(process);
+  } else if (typeof process === 'string') {
+    // If process is a string, convert it to its pid
+    process = processNameToPid(process);
+  }
+
+  return listProcessModules(process);
 }
 
 export function GetModuleHandle(
-  process_name: string,
+  process: string | number | ProcessHandle,
   module_name: string
 ): ModuleHandle {
-  return getModuleHandle(process_name, module_name);
+  // If process is a ProcessHandle, convert it to a pid
+  if (typeof process !== 'number' && typeof process !== 'string') {
+    process = processHandleToPid(process);
+  }
+
+  // If process is a string, do nothing
+  if (typeof process === 'string') {
+    // Do nothing
+  } else if (typeof process === 'number') {
+    // If process is a number, convert it to its process name
+    process = processPidToName(process);
+  }
+
+  return getModuleHandle(process, module_name);
 }
 
 export function GetModuleInfo(
   process_handle: ProcessHandle,
   module_handle: ModuleHandle
-): JSLPMODULEINFO {
+): Jslpmoduleinfo {
   return getModuleInformation(process_handle, module_handle);
 }
 
--- test/module.spec.ts
+++ test/module.spec.ts
@@ -3,7 +3,7 @@ import { OpenProcess } from '../src/process';
 
 // modify timeout to 10 minutes
 jest.setTimeout(600000);
-const PROCESS_NAME = 'notepad.exe';
+const PROCESS_NAME = 'Notepad.exe';
 
 describe('module information', () => {
   test('Get module handle', () => {
@@ -25,6 +25,28 @@ describe('module information', () => {
 
     expect(modules).not.toBeUndefined();
     expect(modules.length).toBeGreaterThan(0);
+  });
+
+  test('Get all process modules with process handle', () => {
+    const processHandle = OpenProcess(PROCESS_NAME);
+    const modules = ListModules(processHandle);
+
+    expect(modules).not.toBeUndefined();
+    expect(modules.length).toBeGreaterThan(0);
+  });
+
+  test('Get all process modules with process name', () => {
+    const modules = ListModules(PROCESS_NAME);
+
+    expect(modules).not.toBeUndefined();
+    expect(modules.length).toBeGreaterThan(0);
+  });
+
+  test('Get module handle from process handle', () => {
+    const processHandle = OpenProcess(PROCESS_NAME);
+    const handle = GetModuleHandle(processHandle, 'textinputframework.dll');
+
+    expect(handle).not.toBeUndefined();
   });
 
   test('Get module information', () => {
