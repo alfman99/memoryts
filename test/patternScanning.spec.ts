10c566a1e9a4a3c213adbae03b60e829daa816db
alfman99 <amanresamntz@gmail.com>
2023-09-07 14:53:02 +0200

    fix pattern scanning test

--- test/patternScanning.spec.ts
+++ test/patternScanning.spec.ts
@@ -1,16 +1,43 @@
+import { FindModule } from '../src/module';
 import { PatternScanning } from '../src/patternScanning';
 import { OpenProcess } from '../src/process';
 
 // modify timeout to 10 minutes
 jest.setTimeout(600000);
-const PROCESS_NAME = 'notepad.exe';
+const PROCESS_NAME = 'Notepad.exe';
 
 describe('patternscanning module', () => {
-  test('Get module handle', () => {
+  test('scan for no wildcards', () => {
     const handle = OpenProcess(PROCESS_NAME);
 
-    const addr = PatternScanning(handle, '69 69 69', 0x0, 0x9999);
+    const moduleInfo = FindModule(handle, 'textinputframework.dll');
+
+    const addr = PatternScanning(
+      handle,
+      '00 00 00',
+      moduleInfo.modBaseAddr,
+      moduleInfo.modBaseAddr + moduleInfo.modBaseSize
+    );
 
     console.log(addr);
+
+    expect(addr).toBeDefined();
+  });
+
+  test('scan with wildcards', () => {
+    const handle = OpenProcess(PROCESS_NAME);
+
+    const moduleInfo = FindModule(handle, 'textinputframework.dll');
+
+    const addr = PatternScanning(
+      handle,
+      '?? 33 ??',
+      moduleInfo.modBaseAddr,
+      moduleInfo.modBaseAddr + moduleInfo.modBaseSize
+    );
+
+    console.log(addr);
+
+    expect(addr).toBeDefined();
   });
 });
